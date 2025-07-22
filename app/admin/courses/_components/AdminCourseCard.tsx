908f8e9c89bbceadaa6c2487014b582d2fb9f50c
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2025-07-22 21:26:08 +0530

    v5- Course creation finished

--- app/admin/courses/_components/AdminCourseCard.tsx
+++ app/admin/courses/_components/AdminCourseCard.tsx
@@ -1,9 +1,25 @@
 import { AdminCourseType } from "@/app/data/admin/admin-get-courses";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useConstructUrl } from "@/hooks/use-construct-url";
-import { ArrowRight, Delete, Eye, MoreVertical, Pencil, School, TimerIcon, Trash2 } from "lucide-react";
+import {
+  ArrowRight,
+  Delete,
+  Eye,
+  MoreVertical,
+  Pencil,
+  School,
+  TimerIcon,
+  Trash2,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -17,36 +33,33 @@ export function AdminCourseCard({ data }: iAppProps) {
     <Card className="group relative py-0 gap-0">
       <div className="absolute top-2 right-2 z-10">
         <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="secondary" size="icon">
-                    <MoreVertical className="size-4"/>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-48">
-                 <DropdownMenuItem asChild>
-                    <Link href={`/admin/courses/${data.id}/edit`}
-                    >
-                        <Pencil className="size-4 mr-2"/>
-                        Edit Course
-                    </Link>
-                 </DropdownMenuItem>
-                 <DropdownMenuItem asChild>
-                    <Link href={`/courses/${data.slug}`}
-                    >
-                        <Eye className="size-4 mr-2"/>
-                        Preview
-                    </Link>
-                 </DropdownMenuItem>
+          <DropdownMenuTrigger asChild>
+            <Button variant="secondary" size="icon">
+              <MoreVertical className="size-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-48">
+            <DropdownMenuItem asChild>
+              <Link href={`/admin/courses/${data.id}/edit`}>
+                <Pencil className="size-4 mr-2" />
+                Edit Course
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <Link href={`/courses/${data.slug}`}>
+                <Eye className="size-4 mr-2" />
+                Preview
+              </Link>
+            </DropdownMenuItem>
 
-                 <DropdownMenuSeparator />
-                    <DropdownMenuItem asChild>
-                    <Link href={`/admin/courses/${data.id}/delete`}
-                    >
-                        <Trash2 className="size-4 mr-2 text-destructive"/>
-                        Delete Course
-                    </Link>
-                 </DropdownMenuItem>
-            </DropdownMenuContent>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem asChild>
+              <Link href={`/admin/courses/${data.id}/delete`}>
+                <Trash2 className="size-4 mr-2 text-destructive" />
+                Delete Course
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
         </DropdownMenu>
       </div>
       <Image
@@ -69,23 +82,56 @@ export function AdminCourseCard({ data }: iAppProps) {
         </p>
 
         <div className="mt-4 flex items-center gap-x-5">
-            <div className="flex items-center gap-2">
-                <TimerIcon className="size-6 p-1 rounded-md text-primary bg-primary/10"/>
-                <p className="text-sm text-muted-foreground">{data.duration}h</p>
-            </div>
-            <div className="flex items-center gap-2">
-                <School className="size-6 p-1 rounded-md text-primary bg-primary/10"/>
-                <p className="text-sm text-muted-foreground">{data.level}</p>
-            </div>
+          <div className="flex items-center gap-2">
+            <TimerIcon className="size-6 p-1 rounded-md text-primary bg-primary/10" />
+            <p className="text-sm text-muted-foreground">{data.duration}h</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <School className="size-6 p-1 rounded-md text-primary bg-primary/10" />
+            <p className="text-sm text-muted-foreground">{data.level}</p>
+          </div>
         </div>
 
-        <Link 
-        className={buttonVariants({
-            className: "w-full mt-4"
-        })}
-        href={`/admin/courses/${data.id}/edit`}>
-        Edit Course <ArrowRight className="size-4"/>
+        <Link
+          className={buttonVariants({
+            className: "w-full mt-4",
+          })}
+          href={`/admin/courses/${data.id}/edit`}
+        >
+          Edit Course <ArrowRight className="size-4" />
         </Link>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default function AdminCourseCardSkeleton() {
+  return (
+    <Card className="group relative py-0 gap-0">
+      <div className="absolute top-2 right-2 z-10 flex items-center">
+        <Skeleton className="h-6 w-16 rounded-full" />
+        <Skeleton className="size-8 rounded-md" />
+      </div>
+      <div className="w-full relative h-fit">
+        <Skeleton className="w-full rounded-lg aspect-video h-[250px] object-cover" />
+      </div>
+
+      <CardContent className="p-4">
+        <Skeleton className="h-6 w-3/4 mb-2 rounded" />
+        <Skeleton className="h-4 w-full mb-4 rounded" />
+        <div className="mt-4 flex items-center gap-x-5">
+          <div className="flex items-center gap-x-2">
+            <Skeleton className="size-6 rounded-md" />
+            <Skeleton className="h-4 w-10rounded" />
+          </div>
+
+          <div className="flex items-center gap-x-2">
+            <Skeleton className="size-6 rounded-md" />
+            <Skeleton className="h-4 w-10rounded" />
+          </div>
+        </div>
+
+        <Skeleton className="mt-4 h-10 w-full rounded"/>
       </CardContent>
     </Card>
   );
