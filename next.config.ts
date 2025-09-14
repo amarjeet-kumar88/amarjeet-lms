dc4460c5c4004919b8c75c6e97b7aa9f1c331c38
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2025-09-14 12:34:42 +0530

    final change

--- next.config.ts
+++ next.config.ts
@@ -6,12 +6,24 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "amarjeet-lms.fly.storage.tigris.dev",
-        port: "", // can be omitted if default (443)
-        pathname: "**", // important to match all image paths
+        port: "",
+        pathname: "**",
       },
     ],
-    dangerouslyAllowSVG: true, // 🔥 this enables remote SVGs
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;", // (optional) tighten security
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+
+  // 👇 yaha fix add kiya
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: [
+        "**/node_modules/**",
+        "**/.git/**",
+        "C:/Users/amarj/Application Data/**", // prevent EPERM error
+      ],
+    };
+    return config;
   },
 };
 
