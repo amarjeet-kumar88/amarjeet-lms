06f614479e49a50ed48a26e5b87fdc1a6e72de19
amarjeet-kumar88 <amarjeetmah12345@gmail.com>
2026-09-11 21:51:20 +0530

    explore courses button work

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
               className={buttonVariants({
                 size: "lg",
               })}
-              href="/cources"
+              href="/courses"
             >
               Explore Cources
             </Link>
